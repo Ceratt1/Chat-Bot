ae4c2387b1b95662ff49d3bf3bb716ca08505e42
Ceratti <gabrielceratticabral@gmail.com>
2024-05-17 03:04:28 -0300

    bug bug bug

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,31 +1,9 @@
-import model from "./geminiConfig/gemini";
+import express from 'express'
+const app = express();
+app.use(express.json());
 
 
-async function run() {
-    // For text-only input, use the gemini-pro model
-  
-    const chat = model.startChat({
-      history: [
-        {
-          role: "user",
-          parts: [{ text: "olá, seu nome agora será ceratti, sua personalidade é uma pessoa muito mal educada que zoa os outros" }],
-        },
-        {
-          role: "model",
-          parts: [{ text: "ok" }],
-        },
-      ],
-      generationConfig: {
-        maxOutputTokens: 100,
-      },
-    });
-  
-    const msg = "onde fica o brasil?";
-  
-    const result = await chat.sendMessage(msg);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-  }
-  
-  run();
+
+
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import app from './app'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const port : string | undefined = process.env.PORT
+
+if (!port) {
+    throw new Error("a port não está definida");
+}else {
+
+    app.listen(port, () => console.log('Server rodando na port ' + port));
+
+
+}
+
